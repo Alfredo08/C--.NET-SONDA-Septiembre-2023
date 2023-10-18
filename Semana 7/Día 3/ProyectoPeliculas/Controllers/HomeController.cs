fa92b91ded5c5a3ca722506bc6571f2e5021630d
Alfredo08 <alfredosv08@gmail.com>
2023-10-18 07:09:04 -0600

    Agregando Modelos

---
+++ Semana 7/Día 3/ProyectoPeliculas/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoPeliculas.Models;
+
+namespace ProyectoPeliculas.Controllers;
+
+public class HomeController : Controller
+{
+    public static List<Director> ListaDirectores = new List<Director>();
+
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger){
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public IActionResult Index(){
+        Director NuevoDirector = new Director(){
+            Id = 123,
+            Nombre = "Alex",
+            Apellido = "Miller"
+        };
+        return View("Index", NuevoDirector);
+    }
+
+    [HttpGet]
+    [Route("privacy")]
+    public IActionResult Privacy()
+    {
+        return View("Privacy");
+    }
+
+    [HttpGet]
+    [Route("directores")]
+    public IActionResult Directores(){
+        return View("Directores", ListaDirectores);
+    }
+
+    [HttpGet]
+    [Route("formulario/director")]
+    public IActionResult FormularioDirector(){
+        return View("FormularioDirector");
+    }
+
+    [HttpPost]
+    [Route("nuevo/director")]
+    public RedirectToActionResult AgregarDirector(Director NuevoDirector){
+        ListaDirectores.Add(NuevoDirector);
+        return RedirectToAction("Directores");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error(){
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Semana 7/Día 3/ProyectoPeliculas/Models/Director.cs
@@ -0,0 +1,8 @@
+#pragma warning disable CS8618
+namespace ProyectoPeliculas.Models;
+
+public class Director{
+    public int Id {get; set;}
+    public string Nombre {get; set;}
+    public string Apellido {get; set;}
+}
